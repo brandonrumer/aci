5c0c98af6d07061e724407dc83167b795782cb2f
brandonrumer <brumer0@gmail.com>
2020-09-02 15:54:11 -0400

    Updated multiple & added getEndpoint data

--- getEndpoints.py
+++ getEndpoints.py
@@ -4,7 +4,13 @@
 """
 
 import requests, json, pprint
+from prettytable import PrettyTable
 from apicLogin import aaaLogin
+
+# Disable insecure certificate warning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 apicUrl = 'sandboxapicdc.cisco.com'
 base_url = f"https://{apicUrl}"
@@ -15,11 +21,32 @@ def getendpoints(cookies):
     endpoint_data = json.loads(response_data.text)
     return endpoint_data
 
-
 if __name__ == "__main__":
+    # Get the login token
     cookies = aaaLogin()
-    
-    endpoints = getendpoints(cookies)
-    
-    pprint.pprint(endpoints)
+
+    # Get the endpoints
+    endpoint_data = getendpoints(cookies)
+    #pprint.pprint(endpoint_data)
+
+
+    # Cleanup the endpoint info
+    fields = ['mac', 'ip', 'dn']
+    data = []
+
+    for endpoint in endpoint_data['imdata']:
+        for stuff in endpoint['fvCEp'].items():
+            line_dict = {}
+            for field in fields:
+                line_dict[field] = stuff[1][field]
+            data.append(line_dict)
+
+    # pprint.pprint(data)
+
+    table = PrettyTable()
+    table.field_names = ['IP Address','MAC Address']
+    for row in data:
+        table.add_row([row['ip'],row['mac']])
+    print(table)
+
 
