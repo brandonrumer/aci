788373bb46f4d57a130f1f8cb5bd92f3e671cf76
brandonrumer <brumer0@gmail.com>
2020-09-03 12:37:37 -0400

    updated getEndpoints with PrettyTable

--- acitoolkit/acitoolkit_getEndpoints.py
+++ acitoolkit/acitoolkit_getEndpoints.py
@@ -4,7 +4,9 @@
 """
 
 import acitoolkit.acitoolkit as aci
-import sys
+import sys, re
+from prettytable import PrettyTable
+
 
 def apicLogin():
     apicUrl = 'sandboxapicdc.cisco.com'
@@ -30,8 +32,31 @@ if __name__ == "__main__":
     endpoints = aci.Endpoint.get(session)
     # print(endpoints)
 
+    data = []
+
     for endpoint in endpoints:
+        if endpoint.if_dn:
+            for dn in endpoint.if_dn:
+                match = re.match('protpaths-(\d+)-(\d+)', dn.split('/')[2])
+                if match:
+                    if match.group(1) and match.group(2):
+                        interface = "Nodes: " + match.group(1) + "-" + match.group(2) + " " + endpoint.if_name
+        else:
+            interface = endpoint.if_name
+        '''
         print(f"MAC:{endpoint.mac}")
         print(f"IP:{endpoint.ip}")
-        print("---------------------")
+        print(f"Interface:{interface}")
+        print("--------------------")
+        '''
+        data_row = {"MAC":endpoint.mac, "IP":endpoint.ip, "Interface":interface}
+        data.append(data_row)
 
+    table = PrettyTable()
+    table.field_names = ["MAC Address", "IP", "Interface"]
+    for row in data:
+        table.add_row([row["MAC"],row["IP"],row["Interface"]])
+    
+    print(table)
+
+    print("\n")
