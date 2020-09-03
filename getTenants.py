af76b533ea5e42f025df6a574ae4874afca351c1
brandonrumer <brumer0@gmail.com>
2020-09-03 15:33:48 -0400

    Made the getTenant a function

--- getTenants.py
+++ getTenants.py
@@ -12,22 +12,14 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-apicUrl = 'sandboxapicdc.cisco.com'
-base_url = f"https://{apicUrl}"
-
-def gettenants(cookies):
+def gettenants(cookies, base_url):
     request_url = '/api/node/class/fvTenant.json'
     response_data = requests.get(base_url + request_url, cookies=cookies, verify=False)
     tenants_data = json.loads(response_data.text)
     return tenants_data
 
 
-if __name__ == "__main__":
-    cookies = aaaLogin()
-    tenants_data = gettenants(cookies)
-    
-    #pprint.pprint(tenants)
-
+def cleanupTenants(tenants_data):
     # Cleanup the endpoint info
     fields = ['name']
     data = []
@@ -39,11 +31,23 @@ if __name__ == "__main__":
                 line_dict[field] = stuff[1][field]
             data.append(line_dict)
 
-    # pprint.pprint(data)
-
     table = PrettyTable()
     table.field_names = ['Tenant Name']
     for row in data:
         table.add_row([row['name']])
+    return(table)
+
+
+def main():
+    apicUrl = 'sandboxapicdc.cisco.com'
+    base_url = f"https://{apicUrl}"
+
+    cookies = aaaLogin()
+    tenants_data = gettenants(cookies, base_url)
+    table = cleanupTenants(tenants_data)
+
     print(table)
 
+
+if __name__ == "__main__":
+    main()
